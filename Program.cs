7f7b79ee4425c6a6c6245f1714b9ae64470c7f92
bellapptech <beladscenter@gmail.com>
2026-06-20 01:49:00 +0200

    Create SQLite database tables on startup

--- Program.cs
+++ Program.cs
@@ -6,10 +6,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(
-        builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlite("Data Source=housingdrey.db"));
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.EnsureCreated();
+}
 
 app.UseStaticFiles();
 
